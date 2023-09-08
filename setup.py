85402824343a7a0443d3e5c7dea278c5e74ceb7a
vu-duy-tung <vuduycun202@gmail.com>
2023-09-08 03:09:12 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='qpth',
+    version='0.0.15',
+    description="A fast and differentiable QP solver for PyTorch.",
+    author='Brandon Amos',
+    author_email='bamos@cs.cmu.edu',
+    platforms=['any'],
+    license="Apache 2.0",
+    url='https://github.com/locuslab/qpth',
+    packages=find_packages(),
+)
